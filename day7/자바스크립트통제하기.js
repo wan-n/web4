4483de54cc437de9a38fdd23e9512310ffd81ecb
soowan kim <tndhks1916@naver.com>
2022-07-20 23:08:41 +0900

    day7-dataset

--- day7/자바스크립트통제하기.js
+++ day7/자바스크립트통제하기.js
@@ -2,6 +2,6 @@
     1. css 클래스를 미리 만들어놓고 
         object.classList.add('클래스이름')
     2. 객체의 style 프로퍼티를 바꿔준다.(인라인 스타일 형태로 삽입된다)
-        적용할요소.style.fontsize = '바꿀값'
+        적용할요소.style.fontSize = '바꿀값'
         (css에서는 font-size이지만 js에서는 fontSize로 사용된다)
 */
