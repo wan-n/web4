cdc6ea1b1287768f62c100230aceb3655bfc2ce9
soowan kim <tndhks1916@naver.com>
2022-07-20 12:56:27 +0900

    day7

---
+++ day7/dom요소가져오기.js
@@ -0,0 +1,14 @@
+//쿼리 셀렉터
+const queryId = document.querySelector('#id');
+const queryClass = document.querySelector('.class');
+
+
+//id
+const getId = document.getElementById('id');
+//class
+const getClass = document.getElementsByClassName('class');
+/*
+    class로 가져올 경우엔 여러개의 동요소를 가져오기 때문에
+    getClass[n]형태로 사용
+*/
+
